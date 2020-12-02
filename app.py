d647bebcba5d53b9e5903d92a224219b3192da03
bh83du <bh83du@student.sunderland.ac.uk>
2020-12-02 10:15:17 +0000

    DB created.  App.py updated to fix case errors

--- app.py
+++ app.py
@@ -1,31 +1,49 @@
 """CETM65 - Week 8 - ePortfolio Task 2"""
 
 import sqlite3
-from Flask import flask, render_template, request, redirect
-from flask_sqlachemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
 # Create a new DB using SQL Alchemy
 
-app.config[SQLALCHEMY_DATABASE_URI] = 'sqlite:///data/portfolio.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/portfolio.db'
+db = SQLAlchemy(app)
 
 # Define db table
 
-class Users(db.model):
-    user_id = db.Column(db.string(6), unique = True, nullable = False)
-    user_name = db.Column(db.string(50), unique = False, nullable = False)
-    dept = db.Column(db.string(20), unique = False, nullable = True)
-    password = db.Column(db.string(12))
-
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.String(6), unique = True, nullable = False)
+    user_name = db.Column(db.String(50), unique = False, nullable = False)
+    dept = db.Column(db.String(20), unique = False, nullable = True)
+    password = db.Column(db.String(12))
 
 # Define __repr__ method for the table
 
-    def __repr__(self)
+    def __repr__(self):
         user_repr = f"User ID: {self.user_id}" \
                     f"Name: {self.user_name}" \
                     f"Department: {self.dept}" \
                     f"Password: {self.password}" \
 
         return user_repr
+
+# Add function for Route 'Sign-Up'
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+
+        req = request.form
+        print(req)
+
+        return redirect('/')
+
+    return render_template('signup.html', title = 'Sign-Up')
+
+
+app.run(debug=True)
+
 
